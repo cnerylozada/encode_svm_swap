6e6024ce8e8cf47d159e61acbd67bc6ad53cc6a3
cnerylozada <cnerylozada@gmail.com>
2025-11-09 17:56:48 -0500

    refact: server commons from env

--- src/server/common.ts
+++ src/server/common.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken'
 import { NextRequest } from 'next/server'
 
-export const BACKEND_AXUM_URL = 'http://localhost:8080/api/v1'
-export const BACKEND_NEXT_URL = 'http://localhost:3000/api'
+export const BACKEND_AXUM_URL = process.env.BACKEND_AXUM_URL!
+export const BACKEND_NEXT_URL = process.env.BACKEND_NEXT_URL!
 
 const AUTH_SECRET = process.env.AUTH_SECRET!
 export const getJWT = async (request: NextRequest) => {
