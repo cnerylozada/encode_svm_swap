16a6858a57f5c39b289f0d63aead891b0047f1e3
cnerylozada <cnerylozada@gmail.com>
2025-11-09 23:32:15 -0500

    feat: basic fetch offer

--- src/app/dashboard/offers/create/page.tsx
+++ src/app/dashboard/offers/create/page.tsx
@@ -1,6 +1,22 @@
+import { SwapContract } from '@/contracts/contracts'
 import { CreateOfferForm } from './_components/CreateOfferForm'
+import { web3 } from '@coral-xyz/anchor'
+import { PublicKey } from '@solana/web3.js'
 
 export default async function Page() {
+  const [pda] = web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from('offer1'),
+      new PublicKey('AKeJdxqP6MpFyhcFGUN79NTUwe2ntZNoGjw37UTbbFp').toBuffer(),
+      Buffer.from('1762745143525'),
+    ],
+    SwapContract.programId,
+  )
+
+  console.log(`pda`, pda)
+  const adasdasd = await SwapContract.account.offer.fetch(pda)
+  console.log(`adasdasd`, adasdasd.tokenOfferedAmount.toNumber())
+
   return (
     <div>
       <CreateOfferForm />
